27bb8dc45c404a02983be8d5242ea483ac4dc854
peso <ray.remedi17@laposte.net>
2019-11-05 20:15:23 +0100

    Route Files With Express Router

---
+++ routes/api/posts.js
@@ -0,0 +1,10 @@
+const express = require('express');
+router = express.Router();
+
+// @route   GET api/posts
+// @desc    Test route
+// @access  Private
+
+router.get('/',(req,res)=> res.send('Posts route'));
+
+module.exports = router;
