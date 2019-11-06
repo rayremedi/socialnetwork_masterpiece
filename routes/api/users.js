94ea6a52210387d94827feb2c7b3ba194232af4d
peso <ray.remedi17@laposte.net>
2019-11-06 18:46:37 +0100

    Implementing JWT (Token)

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -34,7 +36,9 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-       return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
       }
 
       const avatar = gravatar.url(email, {
@@ -47,7 +51,7 @@ router.post(
         name,
         email,
         avatar,
-        password,
+        password
       });
       //Encrypt password
 
@@ -55,8 +59,21 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      // Return jsonwebtoken
-      res.send('User registered');
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
