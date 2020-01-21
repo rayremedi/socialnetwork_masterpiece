3e85eb48a8d3885c804384bb20fd6c895dad0c0a
peso <ray.remedi17@laposte.net>
2020-01-21 17:08:43 +0100

    Post Item Component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,23 +1,40 @@
-import React, {Fragment, useEffect  } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 import { getPosts } from '../../actions/post';
 
-const Posts = ({ getPosts, post:{ posts,loading } }) => {
-    useEffect(()=>{
-        getPosts();
-    }, [getPosts]);
-    return <div/>
-}
+const Posts = ({ getPosts, post: { posts, loading } }) => {
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i>Bienvenue dans la communauté
+      </p>
+      {/* PostForm */}
+      <div className='posts'>
+         
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
+};
 
 Posts.propTypes = {
-getPosts: PropTypes.func.isRequired,
-post: PropTypes.object.isRequired 
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-    post: state.post
+  post: state.post
 });
 
-export default connect(mapStateToProps, {getPosts})(Posts);
+export default connect(mapStateToProps, { getPosts })(Posts);
